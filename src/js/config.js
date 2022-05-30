47ba73d5e6eb29ee79462cfabe26ce81a8a06da5
Tomasz <tomaszniedojadlo92@gmail.com>
2022-05-30 15:15:53 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const API_KEY = '5cf7dcad-bc9e-4182-a591-1e0a5f213627';
+export const MODAL_CLOSE_SEC = 2.5;
